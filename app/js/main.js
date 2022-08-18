127b75a6cdee3ca06ca55c44722ad92e925c4e04
gavrilov <daniil.gavrilooov@gmail.com>
2022-08-18 17:04:09 +0300

    add function to menu  section (header)

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,13 @@
 let burgerBtn = document.querySelector('.header__burger-btn')
 let burgerMenu = document.querySelector('.header__burger-menu')
+let menuItem = document.querySelectorAll('.header__menu-item')
 
 burgerBtn.onclick = function () {
     this.classList.toggle('header__burger-btn--active')
     burgerMenu.classList.toggle('header__burger-menu--active')
 }
+
+menuItem.forEach(item => item.onclick = function() {
+    burgerBtn.classList.remove('header__burger-btn--active')
+    burgerMenu.classList.remove('header__burger-menu--active')
+})
